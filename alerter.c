33252ccf9289f02003eefc4c502cd6fccf93125d
Keerthivasan-Murugesan <keerthim226@gmail.com>
2022-01-24 23:58:35 +0530

    Test update for alerter

--- alerter.c
+++ alerter.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define SW_STUBBED_FOR_TEST
+#define SW_FOR_PRODUCTION
+
+#define SW SW_STUBBED_FOR_TEST
+
 int alertFailureCount = 0;
 
 int networkAlertStub(float celcius) {
@@ -17,6 +22,7 @@ int networkAlertStub(float celcius) {
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
+    #if(SW == SW_STUBBED_FOR_TEST)
     int returnCode = networkAlertStub(celcius);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
@@ -25,6 +31,7 @@ void alertInCelcius(float farenheit) {
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
+    #endif
 }
 
 int main() {
