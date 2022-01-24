0e96da555989a1bdd1b47cb9e8728f9523453451
Keerthivasan-Murugesan <keerthim226@gmail.com>
2022-01-24 23:36:24 +0530

    test update for misaligned

--- misaligned.c
+++ misaligned.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 #include <assert.h>
 
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    colorPair.majorColor = 
+        (enum MajorColor)(pairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(pairNumber % numberOfMinorColors);
+    return colorPair;
+}
+
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor){
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
+
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
@@ -8,6 +31,7 @@ int printColorMap() {
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
             printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            testNumberToPair( i * 5 + j, majorColor[i], minorColor[i]);
         }
     }
     return i * j;
